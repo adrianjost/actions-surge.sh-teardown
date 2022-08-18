633d25c09f5de36b6bab064158f7a47a03efad68
TODO <todo-actions[bot]@users.noreply.github.com>
2022-08-18 23:38:34 +0000

    Update TODO references: #281, #282

--- dist/index.js
+++ dist/index.js
@@ -2137,7 +2137,7 @@ class BearerCredentialHandler {
         this.token = token;
     }
     // currently implements pre-authorization
-    // TODO [$62fecd751430c00009159fff]: support preAuth = false where it hooks on 401
+    // TODO [#281]: support preAuth = false where it hooks on 401
     prepareRequest(options) {
         if (!options.headers) {
             throw Error('The request has no headers');
@@ -2160,7 +2160,7 @@ class PersonalAccessTokenCredentialHandler {
         this.token = token;
     }
     // currently implements pre-authorization
-    // TODO [$62fecd751430c0000915a000]: support preAuth = false where it hooks on 401
+    // TODO [#282]: support preAuth = false where it hooks on 401
     prepareRequest(options) {
         if (!options.headers) {
             throw Error('The request has no headers');
