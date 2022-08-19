58140886be5f5db28e73357d9a2b700866e35af5
GitHub Action <action@github.com>
2022-08-19 12:54:35 +0000

    chore: publish dist

--- dist/index.js
+++ dist/index.js
@@ -2137,7 +2137,7 @@ class BearerCredentialHandler {
         this.token = token;
     }
     // currently implements pre-authorization
-    // TODO [#1149]: support preAuth = false where it hooks on 401
+    // TODO: support preAuth = false where it hooks on 401
     prepareRequest(options) {
         if (!options.headers) {
             throw Error('The request has no headers');
@@ -2160,7 +2160,7 @@ class PersonalAccessTokenCredentialHandler {
         this.token = token;
     }
     // currently implements pre-authorization
-    // TODO [#1150]: support preAuth = false where it hooks on 401
+    // TODO: support preAuth = false where it hooks on 401
     prepareRequest(options) {
         if (!options.headers) {
             throw Error('The request has no headers');
